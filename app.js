974bd8b5197c31e8cc39cdf2c1b190ee617c4d2e
badino25 <nicolaabadino25@gmail.com>
2023-10-20 00:33:49 -0300

    maquetacion

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port  = 7000
+
+app.listen(port, () => {
+    console.log(`Servidor levantado en el puerto ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.use(express.static(__dirname + '/public'));
