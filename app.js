327fb5ae523d26bec5600981c365b222f1c9c9c6
badino25 <nicolaabadino25@gmail.com>
2023-11-20 22:27:54 -0300

    termine formularios

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ app.listen(port, () => {
     console.log(`Servidor levantado en el puerto ${port}`);
 });
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 
-app.get("/", (req, res) => {
+app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
 })
-
+app.post("/register", (req,res) => {
+    console.log(req.body)
+    res.redirect("/login")
+});
 
 app.use(express.static(__dirname + '/public'));
